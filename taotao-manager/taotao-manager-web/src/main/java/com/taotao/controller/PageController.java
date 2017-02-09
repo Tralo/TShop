d1d95c790a37012c2716c4c697b215076a1e8fec
Tralo <13580548169@163.com>
2017-02-09 19:58:55 +0800

    页面跳转实现

---
+++ taotao-manager/taotao-manager-web/src/main/java/com/taotao/controller/PageController.java
@@ -0,0 +1,31 @@
+package com.taotao.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * 页面跳转controller
+ */
+@Controller
+public class PageController {
+	
+	/**
+	 * 打开首页
+	 */
+	@RequestMapping("/")
+	public String index(){
+		return "index";
+	}
+	
+	/**
+	 * 展示其它页面
+	 * @param path
+	 * @return
+	 */
+	@RequestMapping("/{page}")
+	public String showpage(@PathVariable String page){
+		return page;
+	}
+	
+}
